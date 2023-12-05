9121489b82a4e39ea70346123fe9429b06418a44
ScaferuZ <endrafruz@gmail.com>
2023-12-05 13:47:08 +0700

    adding useState value for drawing chart

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,33 @@ function App() {
 
   useEffect(() => {
     //setting up svg
+    const w = 400;
+    const h = 100;
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", w)
+      .attr("height", h)
+      .style("background", "#d3d3d3");
     //setting up the scaling
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, w]);
+    const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]);
+    const generateScaledLine = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y(yScale)
+      .curve(d3.curveCardinal);
     //setting the axes
     //setting up the data for the svg
+    svg
+      .selectAll("line")
+      .data([data])
+      .join("path")
+      .attr("d", (d) => generateScaledLine(d))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
   }, [data]);
 
   return (
